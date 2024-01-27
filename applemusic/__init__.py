8f39e3a9b32dae8080baef9fbe14cd283948625d
Myp3a <murza8965@gmail.com>
2024-01-28 02:11:00 +0300

    fix: __init__ files

--- applemusic/__init__.py
+++ applemusic/__init__.py
@@ -1,5 +1,9 @@
 import logging
 
+from .api import *
 from .client import *
+from .decrypt import *
+from .errors import *
+from .models import *
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
---
+++ applemusic/api/__init__.py
@@ -0,0 +1,5 @@
+from .account import *
+from .catalog import *
+from .library import *
+from .playback import *
+from .playlist import *
